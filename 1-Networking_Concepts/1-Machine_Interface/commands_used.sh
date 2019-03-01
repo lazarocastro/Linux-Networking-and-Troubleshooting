bbc78aadafa7a065f9e0b974a4cc9edfa4f6504a
Lazaro A. S. Castro <lazarocastro@gmail.com>
2019-03-01 16:20:48 -0300

    Networking Concepts | Machine Interface

---
+++ 1-Networking_Concepts/1-Machine_Interface/commands_used.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# show ip address of interfaces
+ip addr show
+# command-line tool for controlling NetworkManager
+nmcli
+# Show device information
+nmcli d show
+# Show table routes
+ip route show
+# List routes with pretty output format
+routel
